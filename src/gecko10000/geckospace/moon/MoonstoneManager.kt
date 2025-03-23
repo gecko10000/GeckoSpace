fee004ec24cc3f901677f402eb5beaee176a6a05
gecko10000 <levtey@gmail.com>
2025-03-23 01:08:28 -0500

    Switch to personal BDM

--- src/gecko10000/geckospace/moon/MoonstoneManager.kt
+++ src/gecko10000/geckospace/moon/MoonstoneManager.kt
@@ -2,6 +2,7 @@ package gecko10000.geckospace.moon
 
 import com.nexomc.nexo.api.NexoBlocks
 import com.nexomc.nexo.api.NexoItems
+import gecko10000.geckolib.blockdata.BlockDataManager
 import gecko10000.geckospace.GeckoSpace
 import gecko10000.geckospace.di.MyKoinComponent
 import gecko10000.geckospace.util.ToolTier
@@ -12,11 +13,11 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.bukkit.Bukkit
 import org.bukkit.Chunk
+import org.bukkit.block.Block
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.world.ChunkLoadEvent
+import org.bukkit.persistence.PersistentDataType
 import org.koin.core.component.inject
-import redempt.redlib.blockdata.BlockDataManager
-import redempt.redlib.blockdata.DataBlock
 import redempt.redlib.misc.EventListener
 import redempt.redlib.misc.Task
 import java.math.BigDecimal
@@ -25,36 +26,32 @@ import kotlin.time.Duration.Companion.seconds
 
 class MoonstoneManager : MyKoinComponent {
 
-    companion object {
-        private const val MOONSTONE_TIME_KEY = "moonstone_time"
-    }
-
     private val plugin: GeckoSpace by inject()
 
-    // TODO: make own generic block data manager (use for player-placed tracking too)
-    // Make type enum, let user pass key prefix, handle events too
-    // Will need to make a cache because want to be able to get all entries (for getLoaded)
-    private val bdm = BlockDataManager.createPDC(plugin, true, true)
+    private val bdm = BlockDataManager("ms", PersistentDataType.LONG)
 
-    private fun growMoonstone(block: DataBlock) {
-        block.remove(MOONSTONE_TIME_KEY)
-        NexoBlocks.place(plugin.config.moonstoneFullId, block.block.location)
+    private fun growMoonstone(block: Block) {
+        bdm.remove(block)
+        NexoBlocks.place(plugin.config.moonstoneFullId, block.location)
     }
 
+    // TODO: optimize?
     private fun tickEmptyMoonstones() {
-        bdm.allLoaded.forEach {
-            if (!it.contains(MOONSTONE_TIME_KEY)) return@forEach
-            it.set(MOONSTONE_TIME_KEY, System.currentTimeMillis())
-            val rarity = plugin.config.moonstoneGrowRarity
-            val didGrow = Random.nextInt(rarity) == 0
-            if (!didGrow) return@forEach
-            growMoonstone(it)
+        for (world in Bukkit.getWorlds()) {
+            for (chunk in world.loadedChunks) {
+                bdm.getValuedBlocks(chunk).forEach {
+                    bdm[it] = System.currentTimeMillis()
+                    val rarity = plugin.config.moonstoneGrowRarity
+                    val didGrow = Random.nextInt(rarity) == 0
+                    if (!didGrow) return@forEach
+                    growMoonstone(it)
+                }
+            }
         }
     }
 
-    private fun catchup(block: DataBlock) {
-        if (!block.contains(MOONSTONE_TIME_KEY)) return
-        val lastUpdate = block.getLong(MOONSTONE_TIME_KEY)
+    private fun catchup(block: Block) {
+        val lastUpdate = bdm.getValue(block)
         val minutesPassed =
             ((System.currentTimeMillis() - lastUpdate) / 1000 / plugin.config.moonstoneGrowSeconds).toInt()
         val noGrowChance = BigDecimal(1 - 1.0 / plugin.config.moonstoneGrowRarity)
@@ -67,14 +64,7 @@ class MoonstoneManager : MyKoinComponent {
     }
 
     private fun updateChunk(chunk: Chunk) {
-        val w = chunk.world
-        val x = chunk.x
-        val z = chunk.z
-        bdm.loadAsync(w, x, z).thenRun {
-            bdm.getLoaded(w, x, z).forEach {
-                catchup(it)
-            }
-        }
+        bdm.getValuedBlocks(chunk).forEach(::catchup)
     }
 
     init {
@@ -97,9 +87,8 @@ class MoonstoneManager : MyKoinComponent {
             // Prevent breaking empty moonstones
             if (nexoId == plugin.config.moonstoneEmptyId) {
                 e.isCancelled = true
-                val dataBlock = bdm.getDataBlock(e.block, true)
-                if (!dataBlock.contains(MOONSTONE_TIME_KEY)) {
-                    dataBlock.set(MOONSTONE_TIME_KEY, System.currentTimeMillis())
+                if (!bdm.contains(e.block)) {
+                    bdm[e.block] = System.currentTimeMillis()
                 }
                 return@EventListener
             }
@@ -108,7 +97,7 @@ class MoonstoneManager : MyKoinComponent {
             if (nexoId == plugin.config.moonstoneFullId) {
                 e.isCancelled = true
                 NexoBlocks.place(plugin.config.moonstoneEmptyId, location)
-                bdm.getDataBlock(e.block, true).set(MOONSTONE_TIME_KEY, System.currentTimeMillis())
+                bdm[e.block] = System.currentTimeMillis()
 
                 val usedItem = e.player.inventory.itemInMainHand
                 val isRightTier = ToolTier.fromItemStack(usedItem)?.isAtLeast(plugin.config.moonstoneMinTier) == true
